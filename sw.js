ba0fbd9d474702cd71207843fe8b14424a68c756
skyblue221 <tjsals0221@gmail.com>
2026-06-05 17:07:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
-const CACHE_NAME = 'barrier-free-v1';
+const CACHE_NAME = 'barrier-free-v6';
 const urlsToCache = [
   '/barrier-free-app/',
   '/barrier-free-app/index.html'
 ];
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
