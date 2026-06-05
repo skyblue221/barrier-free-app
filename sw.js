a2086fd9c9147c1b88cd6e06d9f49558fef5cf14
skyblue221 <tjsals0221@gmail.com>
2026-06-05 23:46:12 +0900

    sw.js 업데이트

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'barrier-free-v6';
+const CACHE_NAME = 'barrier-free-v7';
 const urlsToCache = [
   '/barrier-free-app/',
   '/barrier-free-app/index.html'
@@ -19,8 +19,6 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
